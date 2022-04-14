317a651be44641d855e710ab79828c1a79550866
misairu-0501 <0501.sekine.job@gmail.com>
2022-04-14 08:41:53 +0000

    [Add]public/子供の現在の年齢を表示

--- app/models/child.rb
+++ app/models/child.rb
@@ -8,4 +8,10 @@ class Child < ApplicationRecord
   validates :birthday, presence: true
   validates :gender, presence: true
 
+  #誕生日から年齢を求める
+  def now_age_calculation
+    today = Date.today.strftime("%Y%m%d").to_i
+    birthday = self.birthday.strftime("%Y%m%d").to_i
+    age = ((today - birthday) / 10000).floor
+  end
 end
